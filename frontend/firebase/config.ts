c14d03cc47ff6d7a2ddfec76bcd13565e9809133
nikita-x1 <accounts@nikitax.io>
2025-04-01 14:53:20 -0400

    remove secrets

--- frontend/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCbo5BHBJKwhVblw_8RALdzKWnhCxyHGvI",
-    authDomain: "huro-1b182.firebaseapp.com",
-    projectId: "huro-1b182",
-    storageBucket: "huro-1b182.firebasestorage.app",
-    messagingSenderId: "155701458557",
-    appId: "1:155701458557:web:c930b0728f08f7b66b2314"
-  };
